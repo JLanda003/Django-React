31c9f37c969471b9c529ce80d1e9aa1ab55839aa
Jlandav <98247950+JLanda003@users.noreply.github.com>
2024-04-07 12:28:41 -0500

    //Convertimos la clase a un arrow funtcion
    Esto para poder llevar las nuevas practicas de React.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-export default class App extends Component{
-    constructor(props) {
-        super(props);
-    }
-
-    render(){
-        return(<h1>Probando Codigo de react</h1>)
-    }
+const App = () => {
+  return <h1>Probando Codigo de react</h1>
 }
+
+export default App
 
 const appDiv = document.getElementById('app');
 render(<App />, appDiv)
