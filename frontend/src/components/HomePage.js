b9c60b82e05ffa3e4c78fa0e80e8d8470c97c35d
Jlandav <98247950+JLanda003@users.noreply.github.com>
2024-04-10 23:29:01 -0500

    //Nuevo e Implementacion
    Nuevo: Se crea diseño de pagina principal
    Implementación: Se agrega detección de sesion desde pagina principal con redireccionameinto, a la sala

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Navigate
 } from 'react-router-dom';
 
 import RoomJoinPage from "./RoomJoinPage";
-import CreateRoomPage from "./CreateRoomPage";
+import CreateRoomPage from "./CreateRoomPage";``
 import Room from "./Room";
+import RenderHomePage from "./RenderHomePage";
 
 const HomePage = () => {
+  const [roomDetails, setRoomDetails] = useState({
+    roomCode: null,
+  })
+
+  useEffect ( () => {
+    const getRoom = async() => {
+      try {
+        const response = await fetch('/api/user-in-room')
+        const data = await response.json();
+        data && data.code
+        ? setRoomDetails({roomCode : data.code})
+        : setRoomDetails({roomCode : null});
+      } catch (error) {
+        console.error('Error al procesar la solicitud', error);
+      }
+    };
+    getRoom();
+  }, [])
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<p>This is the home page</p>} />
+        <Route path="/" element={
+          roomDetails.roomCode
+          ? <Navigate to={`/room/${roomDetails.roomCode}`} />
+          : <RenderHomePage />
+        } />
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
---
+++ frontend/src/components/RenderHomePage.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Link,
+} from 'react-router-dom';
+import { Grid, Typography, Button, ButtonGroup } from '@mui/material';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
+
+export default function RenderHomePage() {
+  return(<>
+    <Grid
+      container
+      spacing={3}
+    >
+      <Grid
+        item
+        xs={12}
+        align="center"
+      >
+        <Typography
+          variant="h3"
+          compact="h3"
+        >
+          Reunion
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        align="center"
+      >
+        <ButtonGroup
+          disableElevation
+          variant="outlined"
+          color="secondary">
+            <Button
+              color="secondary"
+              to="/join"
+              component={ Link }
+              startIcon={ <MeetingRoomIcon /> }
+            >
+              Entrar a una Sala
+            </Button>
+            <Button
+              color="secondary"
+              to="/create"
+              component={ Link }
+              endIcon={ <AddBoxIcon /> }
+            >
+              Crear una Sala
+            </Button>
+          </ButtonGroup>
+      </Grid>
+    </Grid>
+  </>);
+}
